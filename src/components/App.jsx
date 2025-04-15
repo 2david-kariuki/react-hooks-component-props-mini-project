91b76cf8b2ad53c804665cc32314324339a4b732
David <david48kariuki@gmail.com>
2025-04-15 22:37:39 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./Header";
+import Header from "./Header.";
 import About from "./About";
 import ArticleList from "./ArticleList";
 import blogData from "../data/blog"; // Assuming blog data is imported
@@ -9,9 +9,9 @@ function App() {
     <div className="App">
       <Header name="Dan's Blog" />
       <aside>
-        <About 
-          image="https://via.placeholder.com/215" 
-          about="This is a blog about React and JavaScript." 
+        <About
+          image="https://via.placeholder.com/215"
+          about="This is a blog about React and JavaScript."
         />
       </aside>
       <main>
